b18f70366bbf719b2909330626a0b9dfbc52afc5
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-05 06:50:23 +0100

    refactor: simplify Home component by removing unused imports and restructuring return

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,5 @@
-import { Stack, Link } from 'expo-router';
-
-import { View } from 'react-native';
-
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
-  return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </View>
-  );
+  return <SafeAreaView className="flex-1"></SafeAreaView>;
 }
-
-const styles = {
-  container: 'flex flex-1 bg-white',
-};
