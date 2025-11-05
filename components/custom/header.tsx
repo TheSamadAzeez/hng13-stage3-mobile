0baecf53496bc759115bfab6c545e08c291dec9d
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-05 09:48:51 +0100

    feat: implement navigation drawer functionality in Header component

--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -1,7 +1,16 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useNavigation } from 'expo-router';
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  const openDrawer = () => {
+    if ('openDrawer' in navigation) {
+      (navigation as any).openDrawer();
+    }
+  };
+
   return (
     <View className="w-screen flex-row items-center justify-between bg-white px-5 pb-9 pt-11">
       {/* logo */}
@@ -10,7 +19,7 @@ export default function Header() {
         <Text className="text-sm font-semibold">Wallpaper Studio</Text>
       </View>
       {/* menu */}
-      <TouchableOpacity activeOpacity={0.7}>
+      <TouchableOpacity activeOpacity={0.8} onPress={openDrawer}>
         <Ionicons name="menu" size={24} color="black" />
       </TouchableOpacity>
     </View>
