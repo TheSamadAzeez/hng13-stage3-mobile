62a3d6b6db39fd39896f4f9da77819d80b167168
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-05 06:50:00 +0100

    refactor: enhance layout component with font loading and splash screen handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,58 @@
+import { Stack } from 'expo-router';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_300Light,
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import Header from '@/components/custom/header';
 import '../global.css';
 
-import { Stack } from 'expo-router';
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_300Light,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: 'Poppins_600SemiBold',
+        },
+        contentStyle: {
+          backgroundColor: '#fff',
+        },
+      }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          header() {
+            return <Header />;
+          },
+        }}
+      />
+    </Stack>
+  );
 }
